ca057ab0e6d4de3d71be170cae173f499ec2fc40
BhetwalAyush <rujanbhetwal@gmail.com>
2024-12-24 11:40:20 +0545

    First_Push

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_test/cubit/student_cubit.dart';
+import 'package:bloc_test/view/student_cubit_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter BLoC',
+      home: BlocProvider(
+        create: (context) => StudentCubit(),
+        child: StudentCubitView(),
+      ),
+    );
+  }
+}
